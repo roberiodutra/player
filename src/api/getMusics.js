9b9b4b8930689c6bcbcd36a565ddfa33abc60ad4
Robério Dutra <dev.roberio@gmail.com>
2024-07-31 15:31:44 -0300

    first commit

--- src/api/getMusics.js
+++ src/api/getMusics.js
@@ -1,10 +1,27 @@
-const YOUTUBE_PLAYLIST_API = "https://www.googleapis.com/youtube/v3/playlistItems";
-let URL = `${YOUTUBE_PLAYLIST_API}?part=snippet&playlistId=PLwhctkjPHK9a2eB7AhUcRQm8h4_854N96&maxResults=50&type=audio&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
+const YOUTUBE_PLAYLIST_API =
+  'https://www.googleapis.com/youtube/v3/playlistItems';
+
+function getPlaylistIdFromUrl() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const playlistId = urlParams.get('playlistId');
+
+  return playlistId;
+}
 
 export default async function getMusics(pageToken) {
+  const playlistId = getPlaylistIdFromUrl();
+
+  if (!playlistId) {
+    console.error('Nenhum playlistId fornecido na URL.');
+    return;
+  }
+
+  let URL = `${YOUTUBE_PLAYLIST_API}?part=snippet&playlistId=${playlistId}&maxResults=50&type=audio&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
+
   if (pageToken) {
     URL = `${URL}&pageToken=${pageToken}`;
   }
+
   const response = await fetch(URL);
   const data = await response.json();
   return data;
